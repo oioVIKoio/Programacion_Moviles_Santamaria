6b5c4e7d55edbe54a7ad0a9fa6c7ebc2d0a54953
oioVIKoio <victorvox2006@gmail.com>
2026-08-28 09:49:45 -0500

    crear modelo Producto para el carrito

--- Semana_02/CarritoSinIa/src/main/java/com/santamaria/carritosinia/Carrito.kt
+++ Semana_02/CarritoSinIa/src/main/java/com/santamaria/carritosinia/Carrito.kt
@@ -1,4 +1,12 @@
 package com.santamaria.carritosinia
 
-class Carrito {
+data class Producto(
+    val id: Int,
+    var nombre: String,
+    var precio: Double,
+    var stock: Int
+)
+
+fun main() {
+    println("=== CARRITO DE PRODUCTOS ===")
 }
