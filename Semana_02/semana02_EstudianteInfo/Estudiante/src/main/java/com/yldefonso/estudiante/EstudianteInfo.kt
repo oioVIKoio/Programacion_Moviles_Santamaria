39effd83e8aae6c747aca1120fdd49d974cbef78
santamaria <santamaria@gmail.com>
2026-08-28 17:03:26 -0500

    implementar calculos de matricula y forma de pago

--- Semana_02/semana02_EstudianteInfo/Estudiante/src/main/java/com/yldefonso/estudiante/EstudianteInfo.kt
+++ Semana_02/semana02_EstudianteInfo/Estudiante/src/main/java/com/yldefonso/estudiante/EstudianteInfo.kt
@@ -1,10 +1,12 @@
-package com.yldefonso.semana02_estudianteinfo
+package com.yldefonso.estudiante
 
 import java.util.Scanner
 
 fun main() {
 
     val scanner = Scanner(System.`in`)
+
+    // DATOS DE ENTRADA
 
     print("Nombre del estudiante: ")
     val estudiante = scanner.nextLine()
@@ -18,6 +20,12 @@ fun main() {
 
     val nombresCursos = Array(cantidadCursos) { "" }
     val creditosCursos = IntArray(cantidadCursos)
+    val costosCursos = DoubleArray(cantidadCursos)
+
+    var totalCreditos = 0
+    var totalPagar = 0.0
+
+    // REGISTRO DE CURSOS
 
     for (i in 1..cantidadCursos) {
 
@@ -29,5 +37,32 @@ fun main() {
         print("Cantidad de creditos: ")
         creditosCursos[i - 1] = scanner.nextInt()
         scanner.nextLine()
+
+        costosCursos[i - 1] = creditosCursos[i - 1] * valorCredito
+
+        totalCreditos += creditosCursos[i - 1]
+        totalPagar += costosCursos[i - 1]
     }
+
+    // CALCULOS
+
+    val cargaAcademica: String
+
+    if (totalCreditos <= 12) {
+        cargaAcademica = "M.R"
+    } else if (totalCreditos <= 18) {
+        cargaAcademica = "CARGA COMPLETA"
+    } else {
+        cargaAcademica = "REQUIERE AUTORIZACION"
+    }
+
+    val cantidadCuotas: Int
+
+    if (totalPagar > 2300) {
+        cantidadCuotas = 3
+    } else {
+        cantidadCuotas = 2
+    }
+
+    val valorCuota = totalPagar / cantidadCuotas
 }
