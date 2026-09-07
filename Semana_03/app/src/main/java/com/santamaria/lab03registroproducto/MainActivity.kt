2c7c54bd7979a0fc09eeaaec5307420851550ef0
oioVIKoio <victorvox2006@gmail.com>
2026-09-07 00:16:09 -0500

    Agrega campos de entrada y estado

--- Semana_03/app/src/main/java/com/santamaria/lab03registroproducto/MainActivity.kt
+++ Semana_03/app/src/main/java/com/santamaria/lab03registroproducto/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,8 +44,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun RegistroProductoScreen(modifier: Modifier = Modifier) {
+
+    var nombreProducto by remember { mutableStateOf("") }
+    var precio by remember { mutableStateOf("") }
+    var cantidad by remember { mutableStateOf("") }
+
     Column(
-        modifier = modifier.padding(16.dp)
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Text(
             text = "Registro de Producto",
@@ -48,5 +64,37 @@ fun RegistroProductoScreen(modifier: Modifier = Modifier) {
             text = "Ingrese los datos del producto",
             fontSize = 16.sp
         )
+
+        OutlinedTextField(
+            value = nombreProducto,
+            onValueChange = { nombreProducto = it },
+            label = {
+                Text("Nombre del producto")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            OutlinedTextField(
+                value = precio,
+                onValueChange = { precio = it },
+                label = {
+                    Text("Precio")
+                },
+                modifier = Modifier.weight(1f)
+            )
+
+            OutlinedTextField(
+                value = cantidad,
+                onValueChange = { cantidad = it },
+                label = {
+                    Text("Cantidad")
+                },
+                modifier = Modifier.weight(1f)
+            )
+        }
     }
 }
