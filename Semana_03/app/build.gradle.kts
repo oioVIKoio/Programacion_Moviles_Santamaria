a0a19d93b22896731c82e94c9813bf8d959fc4de
oioVIKoio <victorvox2006@gmail.com>
2026-09-06 23:58:39 -0500

    Agrega header con jerarquia visual

--- Semana_03/app/build.gradle.kts
+++ Semana_03/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.santamaria.lab03registroproducto"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
--- Semana_03/app/src/main/java/com/santamaria/lab03registroproducto/MainActivity.kt
+++ Semana_03/app/src/main/java/com/santamaria/lab03registroproducto/MainActivity.kt
@@ -4,24 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.santamaria.lab03registroproducto.ui.theme.Lab03RegistroProductoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             Lab03RegistroProductoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    RegistroProductoScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +35,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun RegistroProductoScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
+        Text(
+            text = "Registro de Producto",
+            fontSize = 28.sp
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab03RegistroProductoTheme {
-        Greeting("Android")
+        Text(
+            text = "Ingrese los datos del producto",
+            fontSize = 16.sp
+        )
     }
 }
