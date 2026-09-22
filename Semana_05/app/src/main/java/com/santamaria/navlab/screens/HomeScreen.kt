abd8749dd808ae79700db966c05a70d1910c62d8
oioVIKoio <victorvox2006@gmail.com>
2026-09-22 10:14:13 -0500

    se implemento la Screen Home el cual sería el primero en aparecer para el usuario debido al startDestination de AppNavigation, esta pantalla mostrará la lista y el perfil

--- Semana_05/app/src/main/java/com/santamaria/navlab/screens/HomeScreen.kt
+++ Semana_05/app/src/main/java/com/santamaria/navlab/screens/HomeScreen.kt
@@ -1,2 +1,45 @@
 package com.santamaria.navlab.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.santamaria.navlab.navigation.Screen
+
+@Composable
+fun HomeScreen(navController: NavController) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text("Pantalla Principal")
+
+        Button(
+            onClick = {
+                navController.navigate(Screen.List.route)
+            }
+        ) {
+            Text("VER LISTA")
+        }
+
+        Button(
+            onClick = {
+                navController.navigate(Screen.Profile.route)
+            }
+        ) {
+            Text("MI PERFIL")
+        }
+    }
+}
