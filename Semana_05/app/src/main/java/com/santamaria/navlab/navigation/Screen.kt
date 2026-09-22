6dd508884e4ecc4287ff80c4dab89193eb316961
oioVIKoio <victorvox2006@gmail.com>
2026-09-22 09:58:38 -0500

    implementación de rutas en la sealed class Screen

---
+++ Semana_05/app/src/main/java/com/santamaria/navlab/navigation/Screen.kt
@@ -0,0 +1,11 @@
+package com.santamaria.navlab.navigation
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object List : Screen("list")
+    object Profile : Screen("profile")
+
+    object Detail : Screen("detail/{itemId}") {
+        fun createRoute(itemId: Int): String = "detail/$itemId"
+    }
+}
