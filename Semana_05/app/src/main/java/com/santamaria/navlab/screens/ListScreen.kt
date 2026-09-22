6e97a78750553b740bd9be4256e4980aa6313337
oioVIKoio <victorvox2006@gmail.com>
2026-09-22 10:35:33 -0500

    implementación de la pantalla ListScreen para uso de scallfold y @Optln para comportamiento de scroll en continuas versiones futuras, también se agrego una implementación en dependencias del build.gradle para los iconos (icon extended).

--- Semana_05/app/src/main/java/com/santamaria/navlab/screens/ListScreen.kt
+++ Semana_05/app/src/main/java/com/santamaria/navlab/screens/ListScreen.kt
@@ -1,45 +1,80 @@
 package com.santamaria.navlab.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.santamaria.navlab.navigation.Screen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListScreen(navController: NavController) {
 
-    val elementos = (1..10).toList()
+    val items = (1..8).map {
+        "Elemento número $it"
+    }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
+    Scaffold(
+        topBar = {
 
-        Text("LISTA DE ELEMENTOS")
+            TopAppBar(
+                title = {
+                    Text("Lista")
+                },
 
-        LazyColumn {
+                navigationIcon = {
 
-            items(elementos) { itemId ->
-
-                Text(
-                    text = "Elemento número $itemId",
-                    modifier = Modifier
-                        .clickable {
-                            navController.navigate(
-                                Screen.Detail.createRoute(itemId)
-                            )
+                    IconButton(
+                        onClick = {
+                            navController.popBackStack()
                         }
-                        .padding(16.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Volver"
+                        )
+                    }
+                }
+            )
+        }
+    ) { padding ->
+
+        LazyColumn(
+            contentPadding = padding
+        ) {
+
+            items(items.size) { index ->
+
+                ListItem(
+                    headlineContent = {
+                        Text(items[index])
+                    },
+
+                    supportingContent = {
+                        Text("Toca para ver el detalle")
+                    },
+
+                    modifier = Modifier.clickable {
+
+                        navController.navigate(
+                            Screen.Detail.createRoute(index + 1)
+                        )
+                    }
                 )
+
+                HorizontalDivider()
             }
         }
     }
