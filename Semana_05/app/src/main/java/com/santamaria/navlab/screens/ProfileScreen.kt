ec8f729e803440e92af8dce7a3c096c2913c6374
oioVIKoio <victorvox2006@gmail.com>
2026-09-22 10:41:00 -0500

    Implementación de pantalla del perfil, mostrando al usuario con el control actual, mantiene la capacidad de volver a la pantalla de inicio.

--- Semana_05/app/src/main/java/com/santamaria/navlab/screens/ProfileScreen.kt
+++ Semana_05/app/src/main/java/com/santamaria/navlab/screens/ProfileScreen.kt
@@ -1,2 +1,71 @@
 package com.santamaria.navlab.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.santamaria.navlab.navigation.Screen
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Mi Perfil",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(
+            modifier = Modifier.height(8.dp)
+        )
+
+        Text(
+            text = "Victor Santamaria",
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Text(
+            text = "Este perfil es del usuario actual",
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+
+        Spacer(
+            modifier = Modifier.height(32.dp)
+        )
+
+        Button(
+            onClick = {
+
+                navController.navigate(Screen.Home.route) {
+
+                    popUpTo(Screen.Home.route) {
+                        inclusive = true
+                    }
+                }
+            },
+
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Ir al inicio")
+        }
+    }
+}
