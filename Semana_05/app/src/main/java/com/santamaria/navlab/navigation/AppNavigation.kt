732c53eb94b46d9aeb56f13dd722cf0a00719077
oioVIKoio <victorvox2006@gmail.com>
2026-09-22 10:10:48 -0500

    creación de el control para navegar, aquí se declaro cual screen va a tal ruta de la clase Screen

---
+++ Semana_05/app/src/main/java/com/santamaria/navlab/navigation/AppNavigation.kt
@@ -0,0 +1,54 @@
+package com.santamaria.navlab.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.santamaria.navlab.screens.DetailScreen
+import com.santamaria.navlab.screens.HomeScreen
+import com.santamaria.navlab.screens.ListScreen
+import com.santamaria.navlab.screens.ProfileScreen
+
+@Composable
+fun AppNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument("itemId") {
+                    type = NavType.IntType
+                }
+            )
+        ) { backStackEntry ->
+
+            val itemId =
+                backStackEntry.arguments?.getInt("itemId") ?: 0
+
+            DetailScreen(
+                navController = navController,
+                itemId = itemId
+            )
+        }
+    }
+}
