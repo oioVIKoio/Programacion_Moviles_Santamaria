fd680f6740637062e079da548e37f3d692af36a0
oioVIKoio <victorvox2006@gmail.com>
2026-09-22 10:16:17 -0500

    implementación de ListScreen, en donde podrá visualizarse los elementos, al igual que permitir acceder a su detalle al clickearlo.

--- Semana_05/app/src/main/java/com/santamaria/navlab/screens/ListScreen.kt
+++ Semana_05/app/src/main/java/com/santamaria/navlab/screens/ListScreen.kt
@@ -1,2 +1,46 @@
 package com.santamaria.navlab.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.santamaria.navlab.navigation.Screen
+
+@Composable
+fun ListScreen(navController: NavController) {
+
+    val elementos = (1..10).toList()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+        Text("LISTA DE ELEMENTOS")
+
+        LazyColumn {
+
+            items(elementos) { itemId ->
+
+                Text(
+                    text = "Elemento número $itemId",
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate(
+                                Screen.Detail.createRoute(itemId)
+                            )
+                        }
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
