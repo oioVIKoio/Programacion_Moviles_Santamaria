73013ebe1cce4295ad6318a5ab105ea133f0537a
oioVIKoio <victorvox2006@gmail.com>
2026-09-21 22:26:46 -0500

    implementar LazyColumn inicial para mostrar productos del carrito

--- Semana_04/app/src/main/java/com/santamaria/laboratorio04/MainActivity.kt
+++ Semana_04/app/src/main/java/com/santamaria/laboratorio04/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -106,6 +107,17 @@ fun PantallaCarrito() {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text("Productos: ${productos.size}")
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(productos) { producto ->
+                Text(
+                    text = "${producto.nombre} - S/ ${producto.precio} x ${producto.cantidad}"
+                )
+            }
+        }
     }
 }
