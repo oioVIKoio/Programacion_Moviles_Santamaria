651e49f1c937a1cf770907c392a7301bcd8417d3
oioVIKoio <victorvox2006@gmail.com>
2026-09-22 10:25:50 -0500

    actualización de la pantalla de inicio, los textos y estilo de diseño se ha modificado.

--- Semana_05/app/src/main/java/com/santamaria/navlab/screens/HomeScreen.kt
+++ Semana_05/app/src/main/java/com/santamaria/navlab/screens/HomeScreen.kt
@@ -2,9 +2,14 @@ package com.santamaria.navlab.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,27 +24,36 @@ fun HomeScreen(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(24.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Text("Pantalla Principal")
+        Text(
+            text = "Pantalla Tecsup",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
 
         Button(
             onClick = {
                 navController.navigate(Screen.List.route)
-            }
+            },
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text("VER LISTA")
+            Text("Ver lista de elementos")
         }
 
-        Button(
+        Spacer(modifier = Modifier.height(12.dp))
+
+        OutlinedButton(
             onClick = {
                 navController.navigate(Screen.Profile.route)
-            }
+            },
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text("MI PERFIL")
+            Text("Mi perfil")
         }
     }
 }
