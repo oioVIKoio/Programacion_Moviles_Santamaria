69de1f93605afe8e491fdb2fc0c0120b0b26c45a
oioVIKoio <victorvox2006@gmail.com>
2026-09-23 07:54:29 -0500

    Definir rutas de navegación de Clínica Tecsup para asi poder conectar a traves del AppNavigation los distintos Screens

--- Semana_05/ClinicaTecsup/app/src/main/java/com/santamaria/clinicatecsup/navigation/Screen.kt
+++ Semana_05/ClinicaTecsup/app/src/main/java/com/santamaria/clinicatecsup/navigation/Screen.kt
@@ -1,4 +1,31 @@
 package com.santamaria.clinicatecsup.navigation
 
-class Screen {
+
+sealed class Screen(val route: String) {
+
+    object Home : Screen("home")
+
+    object DoctorDetail : Screen("doctor/{doctorId}") {
+        fun createRoute(doctorId: Int): String = "doctor/$doctorId"
+    }
+
+    object Appointment : Screen("appointment/{doctorId}") {
+        fun createRoute(doctorId: Int): String = "appointment/$doctorId"
+    }
+
+    object Confirmation : Screen(
+        "confirmation/{doctorId}/{fecha}/{hora}"
+    ) {
+        fun createRoute(
+            doctorId: Int,
+            fecha: String,
+            hora: String
+        ): String {
+            return "confirmation/$doctorId/$fecha/$hora"
+        }
+    }
+
+    object Appointments : Screen("appointments")
+
+    object MedicalHistory : Screen("medical_history")
 }
