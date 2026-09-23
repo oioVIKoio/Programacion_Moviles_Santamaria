efffc88ad94d65312526c88284999d8f9e3abcf8
oioVIKoio <victorvox2006@gmail.com>
2026-09-23 07:59:10 -0500

    Se creo el archivo donde se mapearan las rutas para las distintas pantallas del software

--- Semana_05/ClinicaTecsup/app/src/main/java/com/santamaria/clinicatecsup/navigation/AppNavigation.kt
+++ Semana_05/ClinicaTecsup/app/src/main/java/com/santamaria/clinicatecsup/navigation/AppNavigation.kt
@@ -1,2 +1,24 @@
 package com.santamaria.clinicatecsup.navigation
 
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.santamaria.clinica.screens.HomeScreen
+
+@Composable
+fun AppNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+    }
+}
