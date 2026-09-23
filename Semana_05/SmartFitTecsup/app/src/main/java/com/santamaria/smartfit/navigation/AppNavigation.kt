41c7fbac665849f81dc0afd801b6d1528b39326c
oioVIKoio <victorvox2006@gmail.com>
2026-09-23 11:16:54 -0500

    Se agrego el navegador de la app para conectar cada ruta con su screen correspondiente

--- Semana_05/SmartFitTecsup/app/src/main/java/com/santamaria/smartfit/navigation/AppNavigation.kt
+++ Semana_05/SmartFitTecsup/app/src/main/java/com/santamaria/smartfit/navigation/AppNavigation.kt
@@ -1,2 +1,78 @@
 package com.santamaria.smartfit.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.santamaria.smartfit.screens.ClassDetailScreen
+import com.santamaria.smartfit.screens.ConfirmationScreen
+import com.santamaria.smartfit.screens.HomeScreen
+import com.santamaria.smartfit.screens.ProfileScreen
+import com.santamaria.smartfit.screens.ReservationsScreen
+import com.santamaria.smartfit.screens.RoutinesScreen
+
+@Composable
+fun AppNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+
+        composable(
+            route = Screen.ClassDetail.route,
+            arguments = listOf(
+                navArgument("classId") {
+                    type = NavType.IntType
+                }
+            )
+        ) { backStackEntry ->
+
+            val classId =
+                backStackEntry.arguments?.getInt("classId") ?: 0
+
+            ClassDetailScreen(
+                navController = navController,
+                classId = classId
+            )
+        }
+
+        composable(
+            route = Screen.Confirmation.route,
+            arguments = listOf(
+                navArgument("classId") {
+                    type = NavType.IntType
+                }
+            )
+        ) { backStackEntry ->
+
+            val classId =
+                backStackEntry.arguments?.getInt("classId") ?: 0
+
+            ConfirmationScreen(
+                navController = navController,
+                classId = classId
+            )
+        }
+
+        composable(Screen.Reservations.route) {
+            ReservationsScreen(navController)
+        }
+
+        composable(Screen.Routines.route) {
+            RoutinesScreen(navController)
+        }
+
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+    }
+}
