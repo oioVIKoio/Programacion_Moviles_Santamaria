ee1a5de957c1baed14c33e4704024b0dd6bd819e
oioVIKoio <victorvox2006@gmail.com>
2026-09-23 11:20:42 -0500

    Pantalla donde se evidenciará las rutinas asignadas al cliente

--- Semana_05/SmartFitTecsup/app/src/main/java/com/santamaria/smartfit/screens/RoutinesScreen.kt
+++ Semana_05/SmartFitTecsup/app/src/main/java/com/santamaria/smartfit/screens/RoutinesScreen.kt
@@ -1,2 +1,38 @@
 package com.santamaria.smartfit.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.santamaria.smartfit.components.BottomNavigationBar
+
+@Composable
+fun RoutinesScreen(navController: NavController) {
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(navController)
+        }
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
+        ) {
+
+            Text("Mis rutinas")
+
+            Text(
+                text = "Aquí podrás consultar tus rutinas de entrenamiento.",
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
+    }
+}
