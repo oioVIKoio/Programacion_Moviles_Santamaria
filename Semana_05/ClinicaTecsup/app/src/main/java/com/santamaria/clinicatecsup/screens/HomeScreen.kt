5b7d78a90f9cb6d95bcbdb404e36b021be72355e
oioVIKoio <victorvox2006@gmail.com>
2026-09-23 08:00:36 -0500

    Se creo un HomeScreen inicial para que funcione correctamente el flujo del proyecto

--- Semana_05/ClinicaTecsup/app/src/main/java/com/santamaria/clinicatecsup/screens/HomeScreen.kt
+++ Semana_05/ClinicaTecsup/app/src/main/java/com/santamaria/clinicatecsup/screens/HomeScreen.kt
@@ -1,2 +1,10 @@
 package com.santamaria.clinicatecsup.screens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Text("Clínica TECSUP")
+}
